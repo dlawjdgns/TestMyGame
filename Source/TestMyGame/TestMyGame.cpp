8c50deb49fc88225cd16f64bd93ec1f06fcbdae0
LimJungHoon <dlawjd111>
2023-10-06 02:24:44 +0800

    #프로젝트파일 업로드

---
+++ Source/TestMyGame/TestMyGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestMyGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestMyGame, "TestMyGame" );
---
+++ Source/TestMyGame/TestMyGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestMyGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTMYGAME_API ATestMyGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
